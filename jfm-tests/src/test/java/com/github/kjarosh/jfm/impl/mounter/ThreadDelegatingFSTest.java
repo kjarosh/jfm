d1a79e6b072d12f841356e9b2aa2b5f8e241787d
Kamil Jarosz <kamil09875@gmail.com>
2019-05-01 12:53:03 +0200

    Add timeout probing in ThreadDelegatingFSTest
    Depending on a machine the tests are executed on, the code may be
    executing faster or slower. Now the test checks how long a normal
    method is executing in order to set the timeout accordingly.

--- jfm-tests/src/test/java/com/github/kjarosh/jfm/impl/mounter/ThreadDelegatingFSTest.java
+++ jfm-tests/src/test/java/com/github/kjarosh/jfm/impl/mounter/ThreadDelegatingFSTest.java
@@ -1,31 +1,61 @@
 package com.github.kjarosh.jfm.impl.mounter;
 
 import com.github.kjarosh.jfm.tests.util.FuseFSTestBase;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.serce.jnrfuse.ErrorCodes;
 import ru.serce.jnrfuse.FuseFS;
 
 import java.time.Duration;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
  * @author Kamil Jarosz
  */
 class ThreadDelegatingFSTest extends FuseFSTestBase {
-    private static final int TIMEOUT_MILLIS = 80;
+    private static final Logger logger = LoggerFactory.getLogger(ThreadDelegatingFSTest.class);
+
+    private static long timeoutMillis = 80;
 
     @Mock
     private FuseFS inner;
 
     private ThreadDelegatingFS threadDelegatingFS;
+
+    @BeforeAll
+    static void probeMinimumTimeout() {
+        FuseFS testFs = mock(FuseFS.class);
+        FuseFS probingFs = new ThreadDelegatingFS(
+                Executors.newSingleThreadExecutor(), testFs,
+                Duration.ofSeconds(10));
+
+        when(testFs.unlink(any()))
+                .thenAnswer(inv -> {
+                    // simulate method execution
+                    Thread.sleep(5);
+                    return 0;
+                });
+
+        long from = System.nanoTime();
+        probingFs.unlink(null);
+        long time = System.nanoTime() - from;
+
+        timeoutMillis = TimeUnit.NANOSECONDS.toMillis(time * 5);
+        logger.info("Using timeout = " + timeoutMillis);
+    }
 
     @BeforeEach
     void setUpMocks() {
@@ -33,7 +63,7 @@ class ThreadDelegatingFSTest extends FuseFSTestBase {
 
         threadDelegatingFS = new ThreadDelegatingFS(
                 Executors.newSingleThreadExecutor(), inner,
-                Duration.ofMillis(TIMEOUT_MILLIS));
+                Duration.ofMillis(timeoutMillis));
     }
 
     protected Stream<DynamicTest> testMockedMethod(Function<FuseFS, Integer> method) {
@@ -60,7 +90,7 @@ class ThreadDelegatingFSTest extends FuseFSTestBase {
     private void testTimeout(Function<FuseFS, Integer> method) {
         when(method.apply(inner))
                 .thenAnswer(invocation -> {
-                    Thread.sleep(TIMEOUT_MILLIS * 2);
+                    Thread.sleep(timeoutMillis * 2);
                     return 0;
                 });
 
