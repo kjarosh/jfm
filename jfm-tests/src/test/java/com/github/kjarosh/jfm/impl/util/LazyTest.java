d7d060a96c8664b631b1ce43c8a84b3f73283ed5
Kamil Jarosz <kjarosh256@gmail.com>
2020-01-12 13:39:15 +0100

    Add tests

---
+++ jfm-tests/src/test/java/com/github/kjarosh/jfm/impl/util/LazyTest.java
@@ -0,0 +1,63 @@
+package com.github.kjarosh.jfm.impl.util;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Kamil Jarosz
+ */
+class LazyTest {
+    @Test
+    void eagerValue() {
+        Lazy<String, RuntimeException> lazy = Lazy.ofEager("a");
+        assertThat(lazy.get()).isEqualTo("a");
+    }
+
+    @Test
+    void lazyValue() {
+        ThrowingSupplier<String, RuntimeException> supplier = Mockito.mock(ThrowingSupplier.class);
+        when(supplier.get()).thenReturn("a");
+        Lazy<String, RuntimeException> lazy = Lazy.of(supplier);
+
+        verify(supplier, times(0)).get();
+        assertThat(lazy.get()).isEqualTo("a");
+        verify(supplier, times(1)).get();
+        assertThat(lazy.get()).isEqualTo("a");
+        verify(supplier, times(1)).get();
+    }
+
+    @Test
+    void simultaneous() {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Phaser phaser = new Phaser(3);
+
+        ThrowingSupplier<String, RuntimeException> supplier = Mockito.mock(ThrowingSupplier.class);
+        when(supplier.get()).thenReturn("a");
+        Lazy<String, RuntimeException> lazy = Lazy.of(supplier);
+
+        es.submit(() -> {
+            phaser.arriveAndAwaitAdvance();
+            assertThat(lazy.get()).isEqualTo("a");
+            phaser.arriveAndAwaitAdvance();
+        });
+        es.submit(() -> {
+            phaser.arriveAndAwaitAdvance();
+            assertThat(lazy.get()).isEqualTo("a");
+            phaser.arriveAndAwaitAdvance();
+        });
+
+        verify(supplier, times(0)).get();
+        phaser.arriveAndAwaitAdvance();
+        phaser.arriveAndAwaitAdvance();
+        verify(supplier, times(1)).get();
+    }
+}
